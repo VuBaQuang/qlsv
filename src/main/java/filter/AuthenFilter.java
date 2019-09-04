ed5610f174871358f015167b2e6f9a73cba172e2
VuBaQuang <vubaquang1998@gmail.com>
2019-09-04 10:30:08 +0700

    ---update - commit - zing - 4-9

--- src/main/java/filter/AuthenFilter.java
+++ src/main/java/filter/AuthenFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns ={"/faces/admin/*,/faces/user/*"})
+//@WebFilter(urlPatterns ={"/faces/admin/*,/faces/user/*"})
 public class AuthenFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
